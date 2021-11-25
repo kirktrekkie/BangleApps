8233d28288f0b862a2cd63c15d1777d6bceb0a92
kirktrekkie <kirkx10@gmail.com>
2021-11-25 20:00:39 +0100

    Create sections.js

---
+++ apps/sections/app-icon.js
@@ -0,0 +1,1 @@
+require("heatshrink").decompress(atob("mEwgJC/ABEOnkHx/AgEek0E4AFBg0GsHAwAFD4IFCjkDwIdBBYMA4YFCgUAoQFCk1k4wFBj+8s+GJjEfx/H8fx/AFdWH4AlA"))
---
+++ apps/sections/sections.js
@@ -0,0 +1,152 @@
+Graphics.prototype.setFontBebasNeue = function(scale) {
+  // Actual height 40 (41 - 2)
+  g.setFontCustom(atob("AAAAAAAAAAAAAAAfAAAAAAAAfgAAAAAAAPwAAAAAAAH4AAAAAAAD8AAAAAAAB+AAAAAAAA/AAAAAAAAfgAAAAAAAAAAAAAAAAAYAAAAAAAA8AAAAAAAD+AAAAAAAH/AAAAAAAf/gAAAAAA//wAAAAAD//4AAAAAH//gAAAAAf//AAAAAA//8AAAAAD//4AAAAAH//gAAAAAf//AAAAAA//8AAAAAD//4AAAAAD//gAAAAAB//AAAAAAA/8AAAAAAAf4AAAAAAAPgAAAAAAAHAAAAAAAAAAAAAAAAAAB////gAAAH////+AAAH/////gAAH/////4AAH/////+AAD//////AAB//////gAB//////4AA/gAAAH8AAfgAAAB+AAPwAAAA/AAH8AAAA/gAD//////wAA//////wAAf/////4AAP/////8AAD/////8AAA/////8AAAP////8AAAA////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB4AAAAAAAA8AAAAAAAAeAAAAAAAAfAAAAAAAAPgAAAAAAAHwAAAAAAAH/////wAAP/////4AAP/////8AAH/////+AAD//////AAB//////gAA//////wAAf/////4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHgAAH4AAA/4AA/8AAB/8AB/+AAB/+AD//AAB//AD//gAA//gD//wAAf/wH//4AAf/wH//8AAP4AH/h+AAH4AH/g/AAD8AH/gfgAB/AP/APwAA////AH4AAP///AD8AAH///AB+AAD///AA/AAA///AAfgAAP/+AAPwAAD/8AAH4AAAPwAAAAAAAAAAAAAAAAAAAAAAAAAB8AAfgAAAH+AAf+AAAH/AAP/gAAH/gAH/4AAH/wAD/+AAD/4AB//AAB/8Pw//gAB/gH4Af4AA/gD8AH8AAfgB+AB+AAP4B/AB/AAH8B/wA/gAD//////wAA//////wAAf/////4AAP/////8AAD//3//8AAA//x//8AAAH/gf/4AAAAAAA/gAAAAAAAAAAAAAAAAH4AAAAAAAf8AAAAAAA/+AAAAAAB//AAAAAAH//gAAAAAP//wAAAAAf//4AAAAB//78AAAAD//x+AAAAH//A/AAAAP/+AfgAAAf/4APwAAAP/////8AAH/////+AAD//////AAB//////gAA//////wAAf/////4AAP/////8AAAAAAD8AAAAAAAB+AAAAAAAA/AAAAAAAAAAAAAAAAcHwAAAP//+H/AAAH///D/4AAD///h/+AAB///w//gAA///4f/wAAf//8P/4AAP//+D/+AAH4H4AB/AAD8D4AAfgAB+D8AAPwAA/B/AAP4AAfg////8AAPwf///8AAH4P///+AAD8D////AAB+B////AAA/Af///AAAfgH///AAAAAAf/8AAAAAAAAAAAAAAAAAAAAAAA////AAAAD////8AAAH/////gAAH/////4AAH/////+AAD//////AAB//////gAB//////4AA/gH4AH8AAfgH4AB+AAPwD8AA/AAH8B+AA/gAD/A/wB/wAB/8f///wAAf+P///4AAP/H///8AAD/h///8AAA/w///8AAAP4P//8AAAB8A//4AAAAAAAAAAAAAAAAAAAAAH4AAAAAAAD8AAAAAAAB+AAAAAAAA/AAAABwAAfgAAAH4AAPwAAAf8AAH4AAD/+AAD8AAP//AAB+AA///gAA/AH///wAAfgf///wAAPz////AAAH////4AAAD////gAAAB///8AAAAA///wAAAAAf/+AAAAAAP/4AAAAAAH/AAAAAAAD8AAAAAAAAAAAAAAAAAAAAAAAAAAA/wD/8AAAB//H//gAAB//3//4AAB//7//+AAB//////gAA//////wAAf/////4AAf///gP+AAP4B/AB/AAH4AfgAfgAD8APwAPwAB/AP4AP4AA////Af8AAP/////8AAH/////+AAD//////AAA//9///AAAP/+///AAAD/+P//AAAAf4B/+AAAAAAAAAAAAAAAAAAAAAAD//gHgAAAH//8D+AAAH///B/gAAH///w/4AAH///4f+AAD///+P/AAB////H/wAB/gB/gf4AA/gAfwH8AAfgAH4B+AAPwAD4A/AAH8AD8A/gAD//////wAA//////wAAf/////4AAP/////8AAD/////8AAA/////8AAAH////4AAAAf///gAAAAAAAAAAAAAAAAAAAAAAAHwAA+AAAAH4AA/AAAAD8AAfgAAAB+AAPwAAAA/AAH4AAAAfgAD8AAAAPwAB+AAAAH4AA/AAAAAAAAAAA"), 46, atob("ChUWFhYWFhYWFhYWCg=="), 55+(scale<<8)+(1<<16));
+};
+
+Graphics.prototype.setFontMavenPro = function(scale) {
+  // Actual height 18 (17 - 0)
+  g.setFontCustom(atob("AAAAADAAAwAAMAAAAAAcAA/AA/AA/AB+AB+AB+AA8AAABAAH/AD/4B4PA4AwMAODADgwA4OAMB8fAP/gB/wAAAAAAADAABwAAcAAP/8D//A//wAAAAAAAAAAGAMBgHA4BwMA8DA/AwewOfMB/jAPgwAAEAAAAAAAGAMDgDAwwwMMODDDg4wwH+MB//APfgABwAAAAAMAAHAAHwAD8AB7AA8wAf/wP/8D//AAMAADAAAAAAAAB4ID/DA/wwMcMDHDgxw4McMDD/AwfgADwAAAAD/AD/4B/fAdgwOYODGDgxwwMP8DD+AAfAAAAAAAAwAAMAADADAwDwMD8DD8Az8AP8AD4AA4AAAAAA8+Af/wH+cDnDAww4MMODnDAf/wH34AY8AAAAAAAA/AAf4wHOODhjgwcwMHMDhvAf/gD/wAfwAAAAAAAAAAABAgAYYAGGABAgAAAAAA"), 46, atob("BQgNCQwMDAsMCwwMCA=="), 20+(scale<<8)+(1<<16));
+};
+
+Graphics.prototype.setFontExo = function(scale) {
+  // Actual height 16 (15 - 0)
+  g.setFontCustom(atob("AAAAAAAAAAAAAP+w/7AAAPAA8ADwAGAACYAJgAnwP/D/gAnwH/D/gMmACYAIAA4AHwQzhCGEYYdhhiCMIPwAeAAAfAD+AIIAghCCMP7g/8AHAB/we/DmEIYQBhAD8APwAeAj8P4w/BCHEIOwgfCA8ADwAZAAAPAA8AAAAA/4P//4B8ABwADwAz//D/wAACAAIAA8APgAeAA4ACAAAAACAAIAAgAfwB/AAgACAAIAAAAAEAA4ABgBAAEAAQABAAEAABAAMAAQAA4AfAPwH4D8AOAAAAB/4P/wgBCAEIAQgBDAEP/wf+AAAEAAQADAAP/w//AAAAAQgDCAcIDwgdCDkMcQ/hB4EAAAABCAEIYQhhCGEIYQ/jB/8DHgAYAHgA6AOIDwgMCAH/AP8ACAAAA+EP8Q9hCEEIQQhBCGMIfwA+AAAD/gf/DGEIYQhhCGEIYwg/ABwIAAgACAEIDwg+CfAPwA4AAAAAAAeeD/8IYQhhCGEIYQ//D/8AHAEAB8EP4QghCCEIIQwjD/8H/gBgAIEBgwCBAIEBg4CBgAAAIABgAHAAUADQAJgAiACIAAAAAACIAIgAiACIAIgAiACIAIgAAAAAAIgAiADYANAAUABwAGAAIAAABAAMAAg7CDsIMAwgD+ADwAAAAAAA/8n/yQ5pH2kRaRFpE2n+aP9sfmf/w/+AAAABAA8AfgP4D5gOGA/YAfgAPwAHAAAAAA//D/8IYQhhCGEMYQ//B54A8Af+D58MAQgBCAEIAQgBCAEAAA//D/8IAQgBCAEIAQwDB/4D/AAAAAAH/w//CCEIIQghCCEIIQgBAAAH/w//CCAIIAggCCAIIAgAAPAH/g+fDAEIIQghCCEIIQg/AD8AAA//D/8AIAAgACAAIAAgD/8P/wAAAAAP/w//AAEAAQABD/8P/wAAAAAP/w//AGAAcAH4A44OBwwBAAAP/w//AAEAAQABAAEAAQABA/8P/g/AAfgAPwAHAH8D+A+AD/4D/wABAAAP/w//BwABwADwADgAHg//D/8AAADwB/4PjwwBCAEIAQgBDAEP/wf+AGAAAA//D/8IEAgQCBAIMA/wB+AAAADwB/4PjwwBCAEIAcgBzAFv/2f+AGAAAA//D/8IIAggCDAIPA/vB8MAAAOAB8EM4QhhCGEIYQgjCD8AHggACAAIAA//D/8P/wgACAAIAAf4D/4H/wABAAEAAQABAAEP/w/+AAAMAA8AA+AA/AAfAAcAPwH4B8APAAgADAAPwAP8AD8ADwB+B/APgA+AA/gAfwAHAH8H+A+ADAAIAQ4DBw8D3ADwAfgDng8HDAMAAAgADgAHgAHgAH8AfwHgB4AOAAgACAEIBwgPCDkI8QnBD4EOAQwBAAAH/////AAIAA4AB8AB+AA/AAfAAMgADAAP//f/8AAA=="), 32, atob("BAQECwoPCwIFBQcKAwUDBgsGCgkJCgoJCgoDAwoKCgkOCwkJCwkJCgsDBgkIDQsLCgsKCQkLCxAKCgkFBgU="), 16+(scale<<8)+(1<<16));
+};
+
+
+/*
+ * Step counter via widget
+ */
+function getSteps() {
+  if (stepsWidget() !== undefined)
+    return stepsWidget().getSteps();
+  return "???";
+}
+
+function stepsWidget() {
+  if (WIDGETS.activepedom !== undefined) {
+    return WIDGETS.activepedom;
+  } else if (WIDGETS.wpedom !== undefined) {
+    return WIDGETS.wpedom;
+  }
+  return undefined;
+}
+
+// timeout used to update every minute
+var drawTimeout;
+
+// schedule a draw for the next minute
+function queueDraw() {
+  if (drawTimeout) clearTimeout(drawTimeout);
+  drawTimeout = setTimeout(function() {
+    drawTimeout = undefined;
+    draw();
+  }, 60000 - (Date.now() % 60000));
+}
+
+// the rectangles for section divider
+function rect(x, y){
+  g.fillRect(x, y, x+5, y+5);
+}
+
+var old_battery = E.getBattery();
+
+function draw() {
+  //g.setBgColor(0, 1, 0);
+  g.reset();
+
+  // write section headers
+  g.setFont("Exo");
+  g.drawString("TIME", 0, 27, true);
+  g.drawString("BATTERY", 0, 90, true);
+  g.drawString("STEPS", 0, 120, true);
+
+  // work out how to display the current time
+  var d = new Date();
+  var h = d.getHours(), m = d.getMinutes();
+  var time = h + ":" + ("0"+m).substr(-2);
+
+  // draw the time
+  g.setFont("BebasNeue");
+  g.setFontAlign(1,1); // align right bottom
+  g.drawString(time, 165, 85, true /*clear background*/);
+
+  // draw the date, in a normal font
+  g.setFont("6x8", 2);
+  g.setFontAlign(0,1); // align center bottom
+  // pad the date - this clears the background if the date were to change length
+  var dateStr = "    "+require("locale").date(d)+"    ";
+  g.drawString(dateStr, g.getWidth()/2, 170, true /*clear background*/);
+
+  // draw steps
+  g.setFont("MavenPro");
+  let today_step = getSteps();
+  if(today_step == "???"){
+    today_step = "00000";
+  }
+  // today_step = 12345; // TEST
+  g.setFontAlign(0,1);
+  g.drawString(today_step, 145, 136, true);
+
+  // draw battery
+  var battery = E.getBattery();
+  if(old_battery-battery > 5){
+    old_battery = battery;
+  }
+  // old_battery = 91; // TEST
+  g.drawString(old_battery, 97, 107, true);
+
+  // draw battery graphics
+  var bx_offset = 170, by_offset = 88;
+  var b_width = 3, b_height = 14;
+  var b_space = 3;
+
+  var b_bars = old_battery / 10;
+  //var b_bars = 9; // TEST
+  for(var i=0; i<10; i++){
+   if(i>b_bars){
+    g.setColor(1,0,0); // red battery bar red
+   }else{
+    g.setColor(0,1,0); // green battery bar
+   }
+   g.fillRect(bx_offset-(b_width*(i+1))-(b_space*i),
+              by_offset,
+              bx_offset-(b_width*i)-(b_space*i),
+              by_offset+b_height);
+  }
+
+  // draw divider graphics
+  g.setColor(1,0.5,0);
+  for(let i=0;i<20;i++){
+    rect(i*10, 80);
+  }
+  for(let i=0;i<20;i++){
+    rect(i*10, 110);
+  }
+  for(let i=0;i<20;i++){
+    rect(i*10, 140);
+  }
+
+  // queue draw in one minute
+  queueDraw();
+}
+
+Bangle.loadWidgets();
+
+// Clear the screen once, at startup
+g.clear();
+// draw immediately at first
+draw();
+// Stop updates when LCD is off, restart when on
+Bangle.on('lcdPower',on=>{
+  if (on) {
+    draw(); // draw immediately, queue redraw
+  } else { // stop draw timer
+    if (drawTimeout) clearTimeout(drawTimeout);
+    drawTimeout = undefined;
+  }
+});
+
+
+Bangle.drawWidgets();
+Bangle.setUI("clock");
